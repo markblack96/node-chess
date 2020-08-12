da62d6ca8780221058db078e7b7dd434063c2ece
markblack96 <markblack96@gmail.com>
2020-08-12 16:29:15 -0500

    Fixed issues

--- src/app.js
+++ src/app.js
@@ -1,32 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+
 class RoomList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {rooms: []};
+  }
   componentDidMount() {
-    fetch('/rooms')
-      .then(resp=>resp.json())
-      .then(data=>this.props.rooms = data);
-    console.log(this.props.rooms);
+    fetch('/rooms').then(d=>d.json())
+      .then(d=>{
+        console.log("setting state");
+        this.setState({
+          rooms: d
+        })
+        console.log(this.state);
+        this.forceUpdate();
+      });
   }
   render() {
+
+    console.log("Creating list elements");
+    let list = this.state.rooms.map((d)=>{
+      return <li>{d.name}</li>
+    })
+    console.log("List", list);
+    console.log("State", this.state);
     return (
-      <ul>
-        <li></li>
-      </ul>
+      <>
+      <ul>{list}</ul>
+      </>
     )
   }
+  
 }
 
 class App extends React.Component {
   render() {
+    // let rooms = [{id: 1, room: "Hello, room"}];
     return ( 
       <>
-      <RoomList />
       <h1>Welcome to Node-Chess!</h1>
       <p>Node-Chess allows you to make a room and play chess with your friends!</p>
+      <RoomList />
       </>
     ) 
   }
 }
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+
+
+ReactDOM.render(<App />, document.querySelector('#root'))
